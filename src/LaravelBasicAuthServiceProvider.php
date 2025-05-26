45a5130a53d1b2e9f88196473a58b7892772b175
Justin Preuß <justin.preuss@aks-service.de>
2025-05-26 15:38:53 +0200

    Registers middleware using pushMiddleware
    Uses pushMiddleware instead of manually manipulating the middleware stack.
    This provides a more robust and reliable way to register the LaravelBasicAuthMiddleware.

--- src/LaravelBasicAuthServiceProvider.php
+++ src/LaravelBasicAuthServiceProvider.php
@@ -3,19 +3,20 @@
 namespace AksService\LaravelBasicAuth;
 
 use AksService\LaravelBasicAuth\Http\Middleware\LaravelBasicAuthMiddleware;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Symfony\Component\HttpKernel\HttpKernel;
 
 class LaravelBasicAuthServiceProvider extends PackageServiceProvider
 {
+
+    /**
+     * @throws BindingResolutionException
+     */
     public function boot(): LaravelBasicAuthServiceProvider
     {
-        $this->app->booted(function ($app) {
-            $middleware = $app->make(HttpKernel::class)->getMiddleware();
-            $middleware[] = LaravelBasicAuthMiddleware::class;
-            $app->make(HttpKernel::class)->setMiddleware($middleware);
-        });
+        $this->app->make(HttpKernel::class)->pushMiddleware(LaravelBasicAuthMiddleware::class);
 
         return parent::boot();
     }
